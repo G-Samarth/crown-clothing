0a3d2d5c65b9ec41e77662b7058557d35db6e0f0
Samarth Ghulyani <g.samarth@outlook.com>
2020-08-11 19:02:41 +0530

    add redux, implement userReducer and userActions to header and app components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,8 +3,9 @@ import './header.styles.scss';
 import {ReactComponent as Logo } from '../../assets/crown.svg';
 import {Link} from 'react-router-dom';
 import {auth} from '../../firebase/firebase.utils';
+import {connect} from 'react-redux';
 
-const Header = () => (
+const Header = ({currentUser}) => (
     <div className='header'>
         <Link className='logo-container' to="/">
             <Logo className='logo'/>
@@ -22,4 +23,8 @@ const Header = () => (
     </div>
 );
 
-export default Header;
+const mapStateToProps = state => ({
+    currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps)(Header);
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -27,7 +27,7 @@ class SignIn extends React.Component{
                 email: '',
                 password: ''
             });
-            
+
         }catch(error){
             console.error(error);
         }
@@ -69,7 +69,7 @@ class SignIn extends React.Component{
                         <CustomButton type='submit'>
                             sign in
                         </CustomButton>
-                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+                        <CustomButton type='button' onClick={signInWithGoogle} isGoogleSignIn>
                             sign in with google
                         </CustomButton>
                     </div>
